872a9ecc01b9d4cd62bbb0ed88cf4107971a43f3
Hippolyte EZIAN <polarusdev@gmail.com>
2022-12-21 21:24:51 +0100

    Co-authored-by: Ranya <ranya-m@users.noreply.github.com> Co-authored-by: Yassine Mghazli <Yassine-MG@users.noreply.github.com> Co-authored-by: BELGAILA Mohamed Zakaria <BMZ89@users.noreply.github.com> Co-authored-by: Sophia727 <Sophia727@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
-let form = document.forms[0];
-let data;
-// fetchWeather data
+// https://type.fit/api/quotes
+let quoteElement = document.querySelector(".quote");
+let authorElement = document.querySelector(".author");
+let quotes = [];
 
-function fecthData(cityName) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "14469e0a3dmshbdffb26631da8cap1cf0bejsn08b48a710fab",
-      "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
-    },
-  };
+//activer le bouton
+//fetch data de l'url api
+//créer une fonction qui sert a integrer le resultat recherché dans l'html
 
-  fetch(`https://open-weather13.p.rapidapi.com/city/${cityName}`, options)
+let btn = document.querySelector(".new-quote");
+
+function getquotes() {
+  //declare  var where stoccking info
+  fetch(`https://type.fit/api/quotes`)
     .then((response) => {
-      // console.log(response);
       return response.json();
     })
     .then((response) => {
-      console.log(response);
-      data = response;
-
-      document.querySelector(".city-name span").textContent = data.name;
-      let ems = document.querySelectorAll(".city-info em");
-      ems[0].textContent = `Temperature ${(data.main.temp - 32) * (5 / 9)} °C`;
-      ems[1].textContent = `Pression ${data.main.pressure} bars`;
-      ems[2].textContent = `humidite ${data.main.humidity} %`;
+      quotes = response;
+      let quote = quotes[Math.floor(Math.random() * quotes.length)];
+      quoteElement.textContent = quote.text;
+      authorElement.textContent = quote.author;
     })
-    .catch((err) => console.error(err));
+    .catch((error) => console.log(error));
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let input = form.elements["search"];
-  if (input.value.trim() !== "") {
-    fecthData(input.value);
-    console.log(data);
-  }
-});
-
-/* 
-{coord: {…}, weather: Array(1), base: 'stations', main: {…}, visibility: 10000, …}base: "stations"clouds: all: 0[[Prototype]]: Objectcod: 200coord: lat: 35.5711lon: -5.3724[[Prototype]]: Objectdt: 1671478750id: 2528910main: feels_like: 57.33humidity: 82pressure: 1026temp: 57.97temp_max: 57.97temp_min: 57.97[[Prototype]]: Objectname: "Tétouan"sys: country: "MA"id: 2415sunrise: 1671434761sunset: 1671469862type: 1[[Prototype]]: Objecttimezone: 3600visibility: 10000weather: Array(1)0: {id: 800, main: 'Clear', description: 'clear sky', icon: '01n'}length: 1[[Prototype]]: Array(0)wind: deg: 350speed: 5.75[[Prototype]]: Object[[Prototype]]: Object
-
+/*  
+async allow to define asynchronose function and it returns a promise
+ await is always use within async function and it allows to wait until the response is available
 */
+async function loadQuote() {
+  // use try catch to handle error and prevent the code to crach
+  try {
+    let response = await fetch(`https://type.fit/api/quotes`);
+    resultat = await response.json();
+    quotes = resultat;
+    let quote = quotes[Math.floor(Math.random() * quotes.length)];
+    quoteElement.textContent = quote.text;
+    authorElement.textContent = quote.author == null ? "Inconnu" : quote.author;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// load quote when the file is loaded
+// getquotes();
+loadQuote();
+
+btn.addEventListener("click", loadQuote);
